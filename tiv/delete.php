8d9a3465ca7c58d352682c33575e23b0d99326fb
yannig.perre <yannig.perre@gmail.com>
2012-08-12 13:55:36 +0000

    Correction d'une coquille sur la recuperation de la valeur de element.

--- tiv/delete.php
+++ tiv/delete.php
@@ -1,6 +1,4 @@
 <?php
-$title = "Suppression $element - club Aqua Sénart";
-include_once('head.inc.php');
 if(array_key_exists("element", $_GET)) {
   $element = $_GET['element'];
   $id = $_GET['id'];
@@ -8,6 +6,9 @@ if(array_key_exists("element", $_GET)) {
   $element = $_POST['element'];
   $id = $_POST['id'];
 }
+
+$title = "Suppression $element - club Aqua Sénart";
+include_once('head.inc.php');
 include_once("connect_db.inc.php");
 if(!$db_con->query("DELETE FROM $element WHERE id = '$id'")) {
   print "Erreur de suppression de l'élément $element dans la base de données";
