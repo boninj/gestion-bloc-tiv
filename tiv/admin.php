3dfae762b2bd3a1f0668d8fb43d871021af41816
yannig.perre <yannig.perre@gmail.com>
2012-08-12 15:50:47 +0000

    Ajout d'un multiselect permettant de choisir les TIVs.

--- tiv/admin.php
+++ tiv/admin.php
@@ -12,6 +12,11 @@
 <h2>Préparation d'un TIV</h2>
 <script>
   $(function() {
+    $('#choix-tiv').hide();
+    $('#choix-tiv-toggle').click(function() {
+      $('#choix-tiv').toggle(400);
+      return false;
+    });
     $.validator.messages.required = "Champ obligatoire";
     $("#preparation_tiv").validate({
       debug: true,
@@ -36,5 +41,17 @@
 <p>A noter que la préparation d'un TIV consiste à pré-affecter les blocs aux différentes personnes qui feront plus tard le TIV.</p>
 <form name="preparation_tiv" id="preparation_tiv" action="preparation_tiv.php" method="POST">
 <p>Date de préparation du TIV :<input type="text" name="date_tiv" id="date_tiv" size="10" value=""/>
-<input type="submit" name="lancer" value="Procéder à la pré-affectation" /> </p>
+<input type="submit" name="lancer" value="Procéder à la pré-affectation" /></p>
+<p><a href="#" id="choix-tiv-toggle">Choisir les personnes faisant le TIV</a></p>
+<div id="choix-tiv">
+<select id="tivs" name="tivs[]" multiple='multiple'>
+<?php
+include_once("connect_db.inc.php");
+$db_result = $db_con->query("SELECT id,nom,actif FROM inspecteur_tiv ORDER BY nom");
+while($result = $db_result->fetch_array()) {
+  print "<option value='".$result["id"]."'".($result["actif"] === "oui" ? " selected" : "").">".$result["nom"]."</option>\n";
+}
+?>
+</select>
+</div>
 </form>
