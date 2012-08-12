3b33ee05eff90351adebf25e8680892f2e0cd23b
yannig.perre <yannig.perre@gmail.com>
2012-08-12 13:50:42 +0000

    Amélioration du message d'erreur en cas de plantage de l'insertion en base d'un nouvel élément.

--- tiv/ajout_element.php
+++ tiv/ajout_element.php
@@ -5,7 +5,10 @@ $db_result =  $db_con->query("SELECT max(id) + 1 FROM $element;");
 $tmp = $db_result->fetch_array(MYSQLI_NUM);
 $id = $tmp[0];
 if(!$db_con->query("INSERT INTO $element(id) VALUES($id)")) {
+  $title = "Erreur d'insertion d'un élément $element - club Aqua Sénart";
+  include_once('head.inc.php');
   print "Erreur d'insertion du nouvel élément dans la base de données";
+  include_once('foot.inc.php');
   exit(1);
 }
 
