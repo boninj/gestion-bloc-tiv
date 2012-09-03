ab2fc17ffcb9139e07f8333080324754e0667620
yannig.perre <yannig.perre@gmail.com>
2012-09-03 21:18:23 +0000

    Ajout d'une classe permettant de gérer le type personne.

---
+++ tiv/definition_element_personne.inc.php
@@ -0,0 +1,29 @@
+<?php
+class personneElement extends TIVElement {
+  function personneElement($db_con = false) {
+    parent::__construct($db_con);
+    $this->_creation_label = "Création d'une nouvelle personne";
+    $this->_update_label = "Mettre à jour la personne";
+    $this->_elements = array("id" => "Réf.", "nom" => "Prénom Nom", "adresse" => "Adresse de la personne",
+                             "telephone" => "Téléphone de la personne",);
+    $this->_forms = array(
+      "nom"       => array("required", "text",      "Nom/Prénom de la personne"),
+      "adresse"   => array("required", "text", "Adresse de la personne"),
+      "telephone" => array("required", "text", "Téléphone de la personne"),
+    );
+    $this->_forms_rules = '
+  debug: false,
+  rules: {
+    nom: {
+        required: true,
+    },
+    adresse: {
+        required: true,
+    },
+    telephone: {
+        required: true,
+    },
+  }';
+  }
+}
+?>
