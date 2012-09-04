8edf2028f31cc6b40f3a5ae83c43c7ac6427020a
yannig.perre <yannig.perre@gmail.com>
2012-09-04 10:27:39 +0000

    Ortografe

--- tiv/personne.php
+++ tiv/personne.php
@@ -2,5 +2,5 @@
 
 <ul>
   <li><a href='affichage_element.php?element=inspecteur_tiv'>Afficher les inspecteurs TIV du club</a></li>
-  <li><a href='affichage_element.php?element=personne'>Afficher les plonguers du club</a></li>
+  <li><a href='affichage_element.php?element=personne'>Afficher les plongeurs du club</a></li>
 </ul>
