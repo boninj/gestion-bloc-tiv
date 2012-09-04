873eb40fcd93c7b8df83483ee8403601e0a6b5df
yannig.perre <yannig.perre@gmail.com>
2012-09-04 10:26:53 +0000

    Regroupement des items par catégories (materiel et personne).

--- tiv/affichage_element.php
+++ tiv/affichage_element.php
@@ -2,8 +2,6 @@
 if(array_key_exists("element", $_GET)) {
   $element = $_GET["element"];
 }
-$title = "Liste des ".$element."s du club";
-include_once('head.inc.php');
 require_once('definition_element.inc.php');
 require_once('connect_db.inc.php');
 
@@ -11,9 +9,15 @@ if(!isset($real_element)) $real_element = $element;
 $element_class = get_element_handler($real_element, $db_con);
 unset($real_element);
 
+$title = "Liste des ".$element."s du club";
+include_once('head.inc.php');
+
 if($element === "inspection_tiv") {
   $element_class->setDate($date_tiv);
 }
-
+?>
+<p><a href='./'>Retour à l'accueil</a></p>
+<?php
 print $element_class->getHTMLTable("liste_$element", $element);
 ?>
+<p><a href='./'>Retour à l'accueil</a></p>
---
+++ tiv/personne.php
@@ -0,0 +1,6 @@
+<h2>Liste des personnes du club</h2>
+
+<ul>
+  <li><a href='affichage_element.php?element=inspecteur_tiv'>Afficher les inspecteurs TIV du club</a></li>
+  <li><a href='affichage_element.php?element=personne'>Afficher les plonguers du club</a></li>
+</ul>
