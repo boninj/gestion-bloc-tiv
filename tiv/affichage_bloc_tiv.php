a499be371230b71125f60c3180ca40188ce2dfcb
yannig.perre <yannig.perre@gmail.com>
2012-09-10 09:44:42 +0000

    Ajout d'icône dans les messages d'erreurs des TIV/réépreuves.

--- tiv/affichage_bloc_tiv.php
+++ tiv/affichage_bloc_tiv.php
@@ -12,12 +12,14 @@ $tiv_month_interval = $tiv_month_count - $tiv_month_count_warn;
 
 print $bloc_element->constructResume("blocs-reepreuve", strtotime("-$epreuve_month_count_warn month"),
                                      "date_derniere_epreuve", "message_important_reepreuve",
+                                     "<img src='images/security-low.png' style='vertical-align:bottom;' /> ".
                                      "__COUNT__ bloc(s) nécessite une ré-épreuve dans moins de $epreuve_month_interval mois.", 'error',
-                                     "Rien à signaler concernant les réépreuves");
+                                     "<img src='images/security-high.png' style='vertical-align:bottom;' /> Rien à signaler concernant les réépreuves");
 
 print $bloc_element->constructResume("blocs-tiv", strtotime("-$tiv_month_count_warn month"),
                                      "date_dernier_tiv", "message_important_tiv",
+                                     "<img src='images/security-low.png' style='vertical-align:bottom;' /> ".
                                      "__COUNT__ bloc(s) nécessite une inspection TIV dans moins de $tiv_month_interval mois.", 'warning',
-                                     "Rien à signaler concernant les inspections TIV");
+                                     "<img src='images/security-high.png' style='vertical-align:bottom;' /> Rien à signaler concernant les inspections TIV");
 
 ?>
