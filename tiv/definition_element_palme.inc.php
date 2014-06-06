f57a4f7bc5f99f8d1007949801cb5f415f0f6195
yannig.perre <yannig.perre@gmail.com>
2014-06-06 17:47:20 +0000

    Ajout de la gestion des palmes.

--- tiv/definition_element_palme.inc.php
+++ tiv/definition_element_palme.inc.php
@@ -5,11 +5,12 @@ class palmeElement extends TIVElement {
     $this->_parent_url       = "./#materiel";
     $this->_parent_url_label = "<img src='images/materiel.png' /> Matériel";
     $this->_creation_label = "Création d'une paire de palme";
-    $this->_update_label = "Mettre à jour les palmes";
+    $this->_update_label = "Mettre à jour la paire de palme";
     $this->_elements = array("id" => "Réf.", "modele" => "Modèle", "taille" => "Taille",
                              "date_achat" => "Date d'achat", "observation" => "Observation/Remarques");
     $this->_forms = array(
       "modele"         => array("required", "number", "Modèle de palme"),
+      "taille"         => array("required", false,    "Taille des palmes"),
       "date_achat"     => array("required", "date",   "Date d'achat des palmes"),
       "observation"    => array("required", "text",   "Observation/Remarques"),
     );
@@ -17,6 +18,9 @@ class palmeElement extends TIVElement {
   debug: true,
   rules: {
     modele: {
+        required: true,
+    },
+    taille: {
         required: true,
     },
     date_achat: {
