20969ac785b8682afde7103b4742e6f4923b42a8
yannig.perre <yannig.perre@gmail.com>
2014-06-06 17:49:47 +0000

    Ajout de la gestion des palmes.

--- tiv/admin.php
+++ tiv/admin.php
@@ -6,6 +6,7 @@ Type d'élément à déclarer :
 <option value='bloc'>Bloc</option>
 <option value='stab'>Gillet Stabilisateur</option>
 <option value='detendeur'>Détendeur</option>
+<option value='palme'>Palme</option>
 <option value='inspecteur_tiv'>Inspecteur TIV</option>
 </select>
 <input type="submit" name="submit" onclick='return(confirm("Procéder à la création ?"));' value="Procéder à la création du nouvel élément"/>
